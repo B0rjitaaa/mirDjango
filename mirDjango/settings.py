99ffab7ac8287c07ba3bf1e528ce0427602a0261
B0rjitaaa <bmcx12@gmail.com>
2017-04-11 21:04:39 +0200

    Add statics and new template.

--- mirDjango/settings.py
+++ mirDjango/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mirDjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['main/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'wsgi', 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
+FIXTURE_DIIRS = ['../main/fixtures']
