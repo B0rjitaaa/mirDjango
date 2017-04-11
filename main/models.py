93319649f96563a7bc7d6bf440d5e0e51f0aa9c3
B0rjitaaa <bmcx12@gmail.com>
2017-04-11 19:32:56 +0200

    Added models and new gitignore.

--- main/models.py
+++ main/models.py
@@ -1,3 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.db import models
 
 # Create your models here.
+class ExceptionMessage(models.Model):
+	"""docstring for Exception"""
+
+	code = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True
+    )
+
+	date = models.DateTimeField(auto_now=False)
+
+	detail = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True
+    )
+
+	def __str__(self):
+		return str(self.date) + self.detail
