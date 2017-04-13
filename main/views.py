51e1f1e4b5f3284c6490a923b5388da7aa19635c
B0rjitaaa <bmcx12@gmail.com>
2017-04-13 23:52:53 +0200

    Test layout and way for ploting dates.

--- main/views.py
+++ main/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from main.models import ExceptionMessage
 # Create your views here.
 
 def index(request):
-	return render(request, 'index.html', {})
+	exceptionMessages = ExceptionMessage.objects.all()
+	response = {
+		'exceptionMessages': exceptionMessages
+	}
+	return render(request, 'index.html', response)
