8638720002b8613c161d88d9631f98b3de9e2942
B0rjitaaa <bmcx12@gmail.com>
2017-04-14 00:54:31 +0200

    Get date from calendar in template.

--- main/views.py
+++ main/views.py
@@ -5,6 +5,8 @@ from main.models import ExceptionMessage
 # Create your views here.
 
 def index(request):
+	# daterangepicker_start & end
+	print("el post:",request.POST.get('daterange'))
 	exceptionMessages = ExceptionMessage.objects.all()
 	response = {
 		'exceptionMessages': exceptionMessages
