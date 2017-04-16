7d5d894084a90c4a7653169272d6dab8d23ec539
B0rjitaaa <bmcx12@gmail.com>
2017-04-16 22:40:26 +0200

    Test passing dates from a dict to template.

--- main/views.py
+++ main/views.py
@@ -2,28 +2,44 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from datetime import datetime
+import json
 
 from main.models import ExceptionMessage
-# Create your views here.
 
 def index(request):
-	# daterangepicker_start & end
-	print("el post:",request.POST.get('daterange'))
 	exceptionMessages = ExceptionMessage.objects.all()
-	# ExceptionMessage.objects.filter(date__range=[start, end])
-	# counts = ExceptionMessage.objects.filter(date__year=dates[0].date.year).count()
-	# dates = ExceptionMessage.objects.filter(date__range=[start, end]).values_list('date', flat=True).distinct()
-	# >>> ExceptionMessage.objects.filter(date__year=dates[0].year, date__month=dates[0].month, date__day=dates[0].day).count()
-	# crear diccionario
-	exceptions = {}
 	response = {
-		'exceptionMessages': exceptionMessages
-	}
+			'exceptionMessages': exceptionMessages
+		}
+	if request.method == 'POST':
+		dates = request.POST.get('daterange')
+		print('El post: ', dates)
+		exceptionMessages = ExceptionMessage.objects.all()
+
+		#Retrieving dates from DB
+		dates_filtered = ExceptionMessage.objects.filter(date__range=[convert_str_to_datetime(dates[:10]), convert_str_to_datetime(dates[13:])]).values_list('date', flat=True).distinct()
+
+		#Count same dates --> date1:3 times .eg
+		exceptionMessagesTimes = {}
+		for i in range (len(dates_filtered)):
+			date_aux = str(dates_filtered[i].year) + '/' + str(dates_filtered[i].month) + '/' + str(dates_filtered[i].day)
+			exceptionMessagesTimes[date_aux] = ExceptionMessage.objects.filter(
+				date__year=dates_filtered[i].year, 
+				date__month=dates_filtered[i].month, 
+				date__day=dates_filtered[i].day).count()
+
+		response = {
+			'exceptionMessagesTimes': exceptionMessagesTimes
+		}
+
+		print("ExceptionMessagestimes: ", exceptionMessagesTimes)
+		
+
 	return render(request, 'index.html', response)
 
 
 def convert_str_to_datetime(date_str):
 	datetime_object = datetime.strptime(date_str, '%m/%d/%Y')
-	return date
+	return datetime_object
 
 
