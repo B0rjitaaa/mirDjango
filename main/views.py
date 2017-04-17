588366f0e4d0244d179b25616cd4bac1d105585d
B0rjitaaa <bmcx12@gmail.com>
2017-04-17 15:22:12 +0200

    Get a good time for retreiving data.

--- main/views.py
+++ main/views.py
@@ -1,37 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from datetime import datetime
+from datetime import datetime, timedelta
 import json
 
 from main.models import ExceptionMessage
 
 
 def index(request):
-	exceptionMessages = ExceptionMessage.objects.all()
-	response = {
-			'exceptionMessages': exceptionMessages
-		}
+	response = {}
 	if request.method == 'POST':
 		dates = request.POST.get('daterange')
 		print('El post: ', dates)
-		exceptionMessages = ExceptionMessage.objects.all()
 
-		#Retrieving dates from DB
-		dates_filtered = ExceptionMessage.objects.filter(
-			date__range=[convert_str_to_datetime(dates[:10]), convert_str_to_datetime(dates[13:])]).values_list('date', flat=True).distinct()
+		dates_calc = getDatetimesSubstract(convert_str_to_datetime(dates[:10]), convert_str_to_datetime(dates[13:]))
 
-		#Count same dates --> date1:3 times .eg
 		exceptionMessagesTimes = {}
-		for i in range (len(dates_filtered)):
-			# date_aux = str(dates_filtered[i].year) + '/' + str(dates_filtered[i].month) + '/' + str(dates_filtered[i].day)
-			exceptionMessagesTimes[dates_filtered[i]] = ExceptionMessage.objects.filter(
-				date__year=dates_filtered[i].year, 
-				date__month=dates_filtered[i].month, 
-				date__day=dates_filtered[i].day).count()
+
+		for i in range(len(dates_calc)):
+			exceptionMessagesTimes[dates_calc[i]] = ExceptionMessage.objects.filter(
+				date__year=dates_calc[i].year, 
+				date__month=dates_calc[i].month, 
+				date__day=dates_calc[i].day).count()
 
 		response = {
-			'exceptionMessagesTimes': exceptionMessagesTimes
+			'exceptionMessagesTimes': exceptionMessagesTimes,
+			'totalExceptionMessages': ExceptionMessage.objects.count()
 		}
 		
 	return render(request, 'index.html', response)
@@ -40,5 +34,15 @@ def index(request):
 def convert_str_to_datetime(date_str):
 	datetime_object = datetime.strptime(date_str, '%m/%d/%Y')
 	return datetime_object
+
+
+def getDatetimesSubstract(d1, d2):
+	dates_calc = []
+	dates_calc.append(d1)
+	dates_calc.append(d2)
+	delta = d2 - d1         # timedelta
+	for i in range(delta.days + 1):
+		dates_calc.append(d1 + timedelta(days=i))
+	return dates_calc
 
 
